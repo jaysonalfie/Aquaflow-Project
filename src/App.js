c9406f0c7b2dc40b0f47456ff89b7967178cde84
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-04-26 14:41:05 +0300

    finished section3 in homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AboutPage from "./pages/AboutPage";
 //creating react router for the elements in the application
 const router = createBrowserRouter(
   createRoutesFromElements(
-    //creatin parent route as main layout will be in every page
+    //creating parent route as main layout will be in every page
   <Route path='/' element={<MainLayout/>}>
     <Route index element={<HomePage/>}/>
     <Route path="/about" element={<AboutPage/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,53 +3,77 @@ import React from "react";
 import Navbar from "./Navbar";
 import "./style.css";
 import deliveryman from "../images/man delivery1.jpg";
-import sportygirl  from "../images/sportygirl.jpg";
+import sportygirl from "../images/sportygirl.jpg";
 import WaterCard from "./WaterCard";
+import ServiceCard from "./ServiceCard";
 
-const Home = () => {
+const Home = (props) => {
   return (
     <div>
       <section className="section1">
         <article>
-        <p className="heading">Each drop will <br/> make you fresh <br/> and cool</p>
+          <p className="heading">
+            Each drop will <br /> make you fresh <br /> and cool
+          </p>
           <p className="water">
             Water is just not a resource.It's the essence of life itself. Join
             as we delve into the depths of this vital element
           </p>
           <button className="btn">Our services</button>
           <button className="btn2">discover</button>
-        
-
-        
-         
-    
         </article>
-        <img src={deliveryman} className="deliveryman"/>
-        </section>
+        <img src={deliveryman} className="deliveryman" />
+      </section>
       <section className="section2">
-       
         <article>
-        <p className="header2"> Water is nothing but life</p>
-         <img src={sportygirl} className="girlwater"/>
-         <p className="lives">Making you lives safer</p>
-         <p className="waterdetails">Welcome to our aquatic oasis. Immerse youself in the boundless beauty and importance of clean drnking   water. From the tranquil streams to the roaring oceans, water is not just a resource...Join us as we celebrate the beauty, importance and wonders of water. Together, let's make waves and champion this precious resource for a brighter, more sustainable future</p>
-         <button className="btn3">Read more</button>
+          <p className="header2"> Water is nothing but life</p>
+          <img src={sportygirl} className="girlwater" />
+          <p className="lives">Making you lives safer</p>
+          <p className="waterdetails">
+            Welcome to our aquatic oasis. Immerse youself in the boundless
+            beauty and importance of clean drnking water. From the tranquil
+            streams to the roaring oceans, water is not just a resource...Join
+            us as we celebrate the beauty, importance and wonders of water.
+            Together, let's make waves and champion this precious resource for a
+            brighter, more sustainable future
+          </p>
+          <button className="btn3">Read more</button>
         </article>
 
         <article className="watersec">
-          <WaterCard/>
-          <WaterCard/>
-          <WaterCard/>
-          <WaterCard/>
+          <WaterCard />
+          <WaterCard />
+          <WaterCard />
+          <WaterCard />
         </article>
       </section>
+
       <section className="section3">
         <div className="Waterservices">
-            <p>Our services</p>
-            <p>we connect you with the best water shops</p>
-            
-        </div>       
+          <div>
+            <p className="header3">Our services</p>
+            <p className="lives2">we connect you with the best water shops</p>
+          </div>
 
+          {/* passing props from home components */}
+          <div className="serviceCards">
+            <ServiceCard
+              shopName="RubyFresh"
+              shopLocation="BuruBuru"
+              shopNo="+254 789908765"
+            />
+            <ServiceCard
+              shopName="WaterStar"
+              shopLocation="BuruBuru"
+              shopNo="+254 712345678"
+            />
+            <ServiceCard
+              shopName="Alfinerals"
+              shopLocation="Outering Rd."
+              shopNo="+254 765790022"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
