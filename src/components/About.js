abacbd0292a598a42d5e25f5ea8d7b98f480c5eb
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-05-06 14:37:00 +0300

    created 4th section in about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./about/Hero";
 import Hero2 from "./about/Hero2";
 import Hero3 from "./about/Hero3";
-
+import HeroBottom from "./about/HeroBottom";
 
 const About =()=>{
     return(
@@ -15,6 +15,9 @@ const About =()=>{
             </section>
             <section>
                 <Hero3/>
+            </section>
+            <section>
+                <HeroBottom/>
             </section>
             
         </div>
