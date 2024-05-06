da76a29399881bb16621dfc4b5506eebf9c696f0
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-05-06 13:06:19 +0300

    created 3rd section in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./about/Hero";
 import Hero2 from "./about/Hero2";
+import Hero3 from "./about/Hero3";
 
 
 const About =()=>{
@@ -11,6 +12,9 @@ const About =()=>{
             </section>
             <section>
                 <Hero2/>
+            </section>
+            <section>
+                <Hero3/>
             </section>
             
         </div>
