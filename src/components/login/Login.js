a83a93608b3a9c5c01197991f217f2d8d106944f
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-05-13 18:12:03 +0300

    log in state and validation

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,40 @@
 import React from "react";
 import logo from "../../images/logo2.GIF";
 import '../about/style2.css'
+import {useForm} from "react-hook-form"
 import { NavLink } from "react-router-dom";
 
 const Login = () => {
+
+  const {register, handleSubmit, formState:{errors} }= useForm;
+  //
+  // fetching from database
+  // const onFormSubmit = async (data) => {
+  //   try {
+  //     // Send a request to the backend API with the form data (username and password)
+  //     const response = await fetch('/api/login', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(data)
+  //     });
+
+  //     const result = await response.json();
+
+  //     if (response.ok) {
+  //       // Handle successful login
+  //       // e.g., store the user data in state or local storage, redirect to dashboard, etc.
+  //     } else {
+  //       // Handle login error
+  //       // e.g., display an error message to the user
+  //     }
+  //   } catch (error) {
+  //     // Handle network or other errors
+  //     console.error('Error:', error);
+  //   }
+  // };
+
   return (
     <div className="form_wrapper">
       <div className="form-header">
@@ -17,7 +48,11 @@ const Login = () => {
             <label>
               Username
               <span>
-                <input type="text" name="username" />
+                <input 
+                type="text" 
+                name="username"
+                {...register("username", {required:"Username is required"})} />
+                {errors.username && <small>{errors.username.message}</small>}
               </span>
             </label>
           </div>
@@ -25,7 +60,13 @@ const Login = () => {
             <label>
               Password
               <span>
-                <input type="text" name="password" />
+                <input
+                 type="password" 
+                 name="password"
+                 {...register("password", {required:"Password is required"})} />
+
+               {errors.username && <small>{errors.password.message}</small>}
+                
               </span>
             </label>
           </div>
