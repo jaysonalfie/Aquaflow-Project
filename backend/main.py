75fda5638657c413f10fbeb7aa7ef6175a5cd93f
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-05-30 17:43:10 +0300

    created api for user registration authentication

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,41 @@
-from fastapi import FastAPI, HTTPException , Depends, status
-from pydantic import BaseModel
-from typing import Annotated
-import models
-from database import engine, SessionLocal
+from fastapi import FastAPI, Depends, HTTPException,status
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from models import User
+from database import SessionLocal, engine
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
-#creating pydantic models
-#used for data validation
+#The JWT secret and algorithm
+SECRET_KEY = "691e3a14e6a0bb3228b233b5bb2b5743efff3faa82cd64b8eb17f345aabe8c3a"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES= 30
 
-class UserBase(BaseModel):
-    email:str
-    username:str
+#setting up OAuth2 for password0based authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+#list of origins or websited that can talk to your backend
+origins = [
 
-#creating depedancy for the db
+    "http://localhost:3000",
+
+]
+
+#Adding Cross-Origin Resource Sharing to the application (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,#allow onl these origins
+    allow_credentials= True,#allow cookies to be sent
+    allow_methods=["*"],#allow all the HTTP methods
+    allow_headers=["*"], #allow all headers
+)
+
+#creating depedancy for the db to get the database session
 def get_db():
     db= SessionLocal()
     try:
@@ -24,14 +43,97 @@ def get_db():
     finally:
         db.close()
 
-db_dependency = Annotated[Session, Depends(get_db)]
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+#creating pydantic models
+#used for data validation
+class UserCreate(BaseModel):
+    email:str
+    username:str
+    password:str
 
-#endpoint to create new user
-@app.post("/users/", status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserBase, db : Session = Depends(get_db)):
-    db_user = models.User(**user.dict())
+#function to get user by username
+def get_user_by_username(db:Session, username:str):
+    return db.query(User).filter(User.username ==username).first()
+
+#function to create new user in the database
+def create_user(db:Session, user: UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user= User(email= user.email, username=user.username, hashed_password = hashed_password)
     db.add(db_user)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    return "User created"
+
+#endpoint to register a new user
+@app.post("/register")
+def register_user(user : UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return create_user(db =db, user=user)
+
+#authenticationg the user
+def authenticate_user(username:str, password:str, db:Session):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not pwd_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+#creating the access token
+def create_access_token(data:dict, expires_delta:timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+#endpoint to the token
+#will be used to handle login requests and issue tokens
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm= Depends(), db:Session = Depends(get_db)):
+    #authenticating user using username and password
+    user = authenticate_user(form_data.username, form_data.password,db)
+    if not user:
+        raise HTTPException (
+             status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    #sets expiration time for the access token
+    access_token_expires = timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    #creates a jwt token for the authenticated user
+    access_token = create_access_token(
+        data={"sub":user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token":access_token, "token_type": "bearer"}
+
+#function to verify the token
+def verify_token(token:str = Depends(oauth2_scheme)):
+    try:
+        #Decoding the JWT token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        #extracting username from the token's payload
+        username:str = payload.get("sub")
+        #check if username exists in the token's payload
+        if username is None:
+            raise HTTPException(status_code=403, detail="Token is invalid or expired")
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=403,detail = "Token is invalid of expired")
+    
+
+#endpoint to verify the token
+@app.get("/verify-token/{token}")
+async def verify_user_token(token:str):
+    verify_token(token=token)
+    return {"message": "Token is valid"}
+
+
+
+
+
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String
-
+from database import engine
 from database import Base
 
 #defining model for the tables in the db
@@ -8,5 +8,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id= Column(Integer, primary_key=True, index=True)
-    email = Column(String(50), unique=True)
-    username = Column(String(50), unique=True)
+    email = Column(String(50), unique=True ,nullable=True)
+    username = Column(String(50), unique=True,index=True)
+    hashed_password = Column(String(80))
+
+User.metadata.create_all(bind=engine)
